100b6436667dfbbb5d2320ad4193c7b8def7dacf
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-14 14:01:47 -0500

    main js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+
+}
+
+function draw() {
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+
+}
