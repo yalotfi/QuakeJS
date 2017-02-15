f04f020c55faa003aef89aa8fe77e3f209600c44
Yaseen Lotfi <yalotfi@outlook.com>
2017-02-15 15:26:13 -0500

    finish basic viz

--- sketch.js
+++ sketch.js
@@ -1,56 +1,53 @@
-
 // Global Variables
 var mapImage;
 var earthquakes;
+var csvLink = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv';
 
 // Mapbox API things
+var imageURL = 'https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/';
 var centerLat = 0;
 var centerLon = 0;
+var zoom = 1;
 var ww = 1024;
 var hh = 512;
-var imageURL = 'url';
-var token = 'text';
-var zoom = 1;
+var token = '?access_token=pk.eyJ1IjoieWFsb3RmaSIsImEiOiJjaXo3MXg3Y3YwMDRnMzNvajJnMXl3NjdzIn0.D2DxeQZPj3juo7x5FbsQQQ';
 
-// USGS Data
-var csvLink = 'url';
+// Shanghai test point
+/*var lat = 31.2304;
+var lon = 121.4737;*/
 
 function preload() {
-    mapImage = loadImage(imageURL + centerLat + ',' + centerLon ',' + zoom +
-        '/' + ww + 'x' + hh + token);
+    mapImage = loadImage(imageURL + centerLat + ',' + centerLon + ',' + zoom + '/' + ww + 'x' + hh + token);
     earthquakes = loadStrings(csvLink)
 }
-
 
 // Convert longitude to Web-Mercator pixels
 function mercatorX(lon) {
     lon = radians(lon);
-    var a = ((hh/2) / PI) * pow(2, zoom);
+    var a = (256 / PI) * pow(2, zoom);
     var b = lon + PI;
     return a * b;
 }
 
-
 // Convert latitude to Web-Mercator pixels
 function mercatorY(lat) {
     lat = radians(lat);
-    var a = ((hh/2) / PI) * pow(2,zoom);
+    var a = (256 / PI) * pow(2,zoom);
     var b = tan(PI / 4 + lat / 2);
-    var c = PI - log(b)
+    var c = PI - log(b);
     return a * c
 }
 
-
 function setup() {
-    createCanvas(ww, hh);
-    translate(width / 2, height / 2);
+    createCanvas(1024,512);
+    translate(width/2, height/2);
     imageMode(CENTER);
     image(mapImage, 0, 0);
 
     var centerX = mercatorX(centerLon);
     var centerY = mercatorY(centerLat);
 
-    for (var i = 1; i < earthquakes.length; i++) {
+    for (var i = 0; i < earthquakes.length; i++) {
         var data = earthquakes[i].split(/,/);
         var lat = data[1];
         var lon = data[2];
@@ -61,11 +58,11 @@ function setup() {
         mag = pow(10, mag);
         mag = sqrt(mag);
 
-        var maxMag = sqrt(pow(10, 10));
-        var d = map(mag, 0, maxMag, 0, 100);
+        var maxMag = sqrt(pow(10,10));
+        var d = map(mag, 0, maxMag, 0, 180);
 
         stroke(255, 0, 255);
         fill(255, 0, 255, 200);
-        ellipse(x, y, d, d);
+        ellipse(x, y, d, d)
     }
 }
